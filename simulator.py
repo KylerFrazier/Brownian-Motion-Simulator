06c61a8bfd8b4daed5b59a6a9235667bbbd4a4e1
Kyler Frazier <kyfrazier100@gmail.com>
2020-08-23 16:03:06 -0700

    Updated simulator.py variable init

--- simulator.py
+++ simulator.py
@@ -1,38 +1,49 @@
 from tkinter import Tk, Canvas, TclError, NW
 from time import sleep
 from random import random as rand
+from math import inf, pi, sin, cos
+from brownians import ThermalNeutron
 
 class BrownianSimulator:
     
-    def __init__(self, particle_type, detector_orientation = "", 
-                detector_size = 10, max_cycles = 1000, display = True, 
-                w = 1280, h = 720, fps = 24, trace = False):
+    def __init__(self, particle_type = ThermalNeutron, max_cycles = 1000, 
+        x0 = 0, y0 = 0, w0 = 0, h0 = 0, partitions = {10}, 
+        detector_size = 0, detector_orientation = "", det_x = 400, det_y = 0,
+        display = True, w = 1280, h = 720, fps = 100, trace = False
+    ):
         
-        self.display = display
+        self.particle_type = particle_type
+        self.detector_size = detector_size
+        self.partitions = sorted([
+            (tup[0] + w/2, tup[1]) if hasattr(tup, "__len__") and len(tup) > 1 \
+            else (inf, tup) for tup in partitions
+        ])
         self.width = w
         self.height = h
-        self.particle_type = particle_type
+        self.x0 = self.width/2 + x0
+        self.y0 = self.height/2 - y0
+        self.w0 = w0
+        self.h0 = h0
+        self.max_cycles = max_cycles
+        self.display = display
         self.frame_time = int(1000/fps)
         self.trace = trace
+
+        self.detector_orientation = detector_orientation
+        det_angle = detector_orientation * pi / 180
+        self.detector_points = ((
+            self.width/2 + det_x + self.detector_size / 2 * cos(det_angle),
+            self.height/2 - det_y - self.detector_size / 2 * sin(det_angle)), (
+            self.width/2 + det_x - self.detector_size / 2 * cos(det_angle),
+            self.height/2 - det_y + self.detector_size / 2 * sin(det_angle)
+        ))
+        
         self.collisions = 0
         self.cycle = 0
-        self.max_cycles = max_cycles
-        self.detector_size = detector_size
 
-        if detector_orientation.lower() == "horizontal":
-            self.detector_orientation = "Horizontal"
-            self.detector_points = ((self.width/2 - 400 - self.detector_size/2, 
-                                     self.height/2), 
-                                    (self.width/2 - 400 + self.detector_size/2, 
-                                     self.height/2))
-        else:
-            self.detector_orientation = "Vertical"
-            self.detector_points = ((self.width/2 - 400, 
-                                     self.height/2 - self.detector_size/2), 
-                                    (self.width/2 - 400, 
-                                     self.height/2 + self.detector_size/2))
-
-        self.particles = {particle_type(self.width/2, self.height/2) : 0}
+        self.particles = {
+            particle_type(self.x0 + rand()*self.w0, self.y0 + rand()*self.h0)
+        }
 
         if self.display:
             self.gui = Tk()
