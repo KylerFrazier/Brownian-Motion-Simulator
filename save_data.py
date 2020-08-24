060be2a7de50104bb127c415ac83ca71ae70a110
Kyler Frazier <kyfrazier100@gmail.com>
2020-08-23 17:36:50 -0700

    Updated save_data.py

--- save_data.py
+++ save_data.py
@@ -1,35 +1,45 @@
+import os
 from numpy import array, linspace
 from matplotlib import pyplot
-import os
+from datetime import datetime
 
-def save_hv_data(h, v, detector_size, particle_type, trials, max_cycles):
+def save_sim_data(params_list, results, trials):
     
     # Processing Data
 
-    a = array(h)
-    b = array(v)
-    max_total = max(max(h),max(v)) + 1
-    min_total = min(min(h),min(v))
+    max_total = max(i for result in results.values() for i in result) + 1
+    min_total = min(i for result in results.values() for i in result)
     num_bins = max_total - min_total + 1
+
     bins = linspace(min_total, max_total, num_bins)
-    pyplot.hist(a, bins, alpha=0.5, label="Horizontal")
-    pyplot.hist(b, bins, alpha=0.5, label="Vertical")
-    pyplot.legend(loc="upper right")
+    for name, result in results.items():
+        pyplot.hist(array(result), bins, alpha = 0.5, label = name)
+    pyplot.legend(loc = "upper right")
 
     # Saving data
 
-    folder = os.path.join('.', f"output_detectorSize-{detector_size}_avgStep-{particle_type.avg_step_size}_lifeTime-{particle_type.life_time}")
+    folder = os.path.join('.', "output")
     if not os.path.exists(folder):
         os.mkdir(folder)
     
-    i = 1
-    while os.path.exists(f"{folder}/trials-{trials}_cycles-{max_cycles}_({i}).txt"):
-        i += 1
-    pyplot.savefig(f"{folder}/trials-{trials}_cycles-{max_cycles}_({i}).png")
-    with open(f"{folder}/trials-{trials}_cycles-{max_cycles}_({i}).txt", "w") as output:
-        output.write(f"Detector Size: {str(detector_size)}, Particle Type: {particle_type.__name__}, Trials: {trials}, Max Cycles: {max_cycles}\n")
-        output.write("Horizontal Detector:\n")
-        output.write(str(h).strip("[]"))
-        output.write("\nVertical Detector:\n")
-        output.write(str(v).strip("[]"))
+    datetime_str = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+    file_name = f"{folder}/datetime-{datetime_str}_trials-{trials}"
+    if os.path.exists(f"{file_name}.png"):
+        i = 1
+        while os.path.exists(f"{file_name}_({i}).png"):
+            i += 1
+        file_name += f"_({i})"
+    
+    pyplot.savefig(f"{file_name}.png")
+    
+    with open(f"{file_name}.txt", "w") as output:
+        for params in params_list:
+            name = params.pop("name")
+            result = results[name]
+            output.write(f"{name}\nresults : {str(result).strip('[]')}\n")
+            for param_name, param in sorted(params.items()):
+                output.write(f"{param_name} : {str(param)}\n")
+            output.write('\n')
+    
     pyplot.show()
